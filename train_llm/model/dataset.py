6566d596ff4a84e8324214c70330948a49919730
zhangyf <zhangyifan0129@gmail.com>
2025-04-17 18:09:51 +0800

    dpo start

--- train_llm/model/dataset.py
+++ train_llm/model/dataset.py
@@ -120,8 +120,85 @@ class SFTDataset(Dataset):
             'labels': Y,
         }
 
+
+class DPODataset(Dataset):
+    def __init__(self, file_path, tokenizer, max_length=4096):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.padding = tokenizer.pad_token_id if tokenizer.pad_token_id is not None else 0
+        self.bos_id = tokenizer('<s>assistant', add_special_tokens=False).input_ids
+        self.eos_id = tokenizer('</s>', add_special_tokens=False).input_ids
+        with open(file_path, 'r', encoding='utf-8') as f:
+            self.data = []
+            for line in f:
+                line = line.strip()
+                obj = json.loads(line)
+                self.data.append(obj)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        item = self.data[index]
+        chosen = item['chosen']  # 是一个 list，里面包含若干 {role, content}
+        rejected = item['rejected']  # 同上
+        chosen_prompt = self.tokenizer.apply_chat_template(
+            chosen, tokenize=False, add_generation_prompt=False
+        )
+
+        rejected_prompt = self.tokenizer.apply_chat_template(
+            rejected, tokenize=False, add_generation_prompt=False
+        )
+        chosen_encoding = self.tokenizer(
+            chosen_prompt, truncation=True, max_length=self.max_length, padding='max_length'
+        )
+        rejected_encoding = self.tokenizer(
+            rejected_prompt, truncation=True, max_length=self.max_length, padding='max_length'
+        )
+
+        chosen_input_ids = chosen_encoding['input_ids']
+        chosen_loss_mask = self._generate_loss_mask(chosen_input_ids)
+
+        rejected_input_ids = rejected_encoding['input_ids']
+        rejected_loss_mask = self._generate_loss_mask(rejected_input_ids)
+        x_chosen = torch.tensor(chosen_input_ids[:-1], dtype=torch.long)
+        y_chosen = torch.tensor(chosen_input_ids[1:], dtype=torch.long)
+        mask_chosen = torch.tensor(chosen_loss_mask[1:], dtype=torch.long)
+        x_rejected = torch.tensor(rejected_input_ids[:-1], dtype=torch.long)
+        y_rejected = torch.tensor(rejected_input_ids[1:], dtype=torch.long)
+        mask_rejected = torch.tensor(rejected_loss_mask[1:], dtype=torch.long)
+
+        return {
+            'x_chosen': x_chosen,
+            'y_chosen': y_chosen,
+            'mask_chosen': mask_chosen,
+            'x_rejected': x_rejected,
+            'y_rejected': y_rejected,
+            'mask_rejected': mask_rejected
+        }
+
+    def _generate_loss_mask(self, input_ids):
+        loss_mask = [0] * len(input_ids)
+        i = 0
+        while i < len(input_ids):
+            if input_ids[i:i + len(self.bos_id)] == self.bos_id:
+                start = i + len(self.bos_id)
+                end = start
+                while end < len(input_ids):
+                    if input_ids[end:end + len(self.eos_id)] == self.eos_id:
+                        break
+                    end += 1
+                for j in range(start + 1, min(end + len(self.eos_id) + 1, self.max_length)):
+                    loss_mask[j] = 1
+                i = end + len(self.eos_id) if end < len(input_ids) else len(input_ids)
+            else:
+                i += 1
+        return loss_mask
+
+
 if __name__ == '__main__':
-    data_path = "/home/bmh/project/llm_related/train_llm/data/sft_mini_test.jsonl"
-    tokenizer = AutoTokenizer.from_pretrained('/train_llm/model/tokenizer')
-    ds = SFTDataset(data_path, tokenizer)
+    data_path = "/home/bmh/project/llm_related/train_llm/data/dpo/dpo_test.jsonl"
+    tokenizer = AutoTokenizer.from_pretrained('/home/bmh/project/llm_related/train_llm/model/tokenizer')
+    ds = DPODataset(data_path, tokenizer)
     print(ds[0])
--- train_llm/test.py
+++ train_llm/test.py
@@ -3,23 +3,37 @@ import torch
 from model.model import MiniMindLM
 from model.config import LMConfig
 
-# tokenizer = AutoTokenizer.from_pretrained('/home/bmh/project/llm_related/train_llm/model/save/pretrain/minimind_base_pre')
-# AutoConfig.register("minimind", LMConfig)
-# AutoModelForCausalLM.register(LMConfig, MiniMindLM)
-#
-# model = AutoModelForCausalLM.from_pretrained('/home/bmh/project/llm_related/train_llm/model/save/pretrain/minimind_base_pre')
-#
-# input_data = [tokenizer.bos_token_id] + tokenizer.encode('马克思主义基本原理')
+tokenizer = AutoTokenizer.from_pretrained('/home/bmh/project/llm_related/train_llm/model/save/full_sft/minimind_base_sft')
+AutoConfig.register("minimind", LMConfig)
+AutoModelForCausalLM.register(LMConfig, MiniMindLM)
+
+model = AutoModelForCausalLM.from_pretrained('/home/bmh/project/llm_related/train_llm/model/save/full_sft/minimind_base_sft')
+
+# input_data = [tokenizer.bos_token_id] + tokenizer.encode('1+1等于多少')
 # input_ids = torch.tensor(input_data).unsqueeze(0)
-#
+
+input_data = tokenizer.apply_chat_template([{'role':'user', 'content':'1+1等于多少？'}])
+input_ids = torch.tensor(input_data).unsqueeze(0)
+
+outputs = model.generate(
+                input_ids,
+                eos_token_id=tokenizer.eos_token_id,
+                max_new_tokens=1024,
+                temperature=0.85,
+                top_p=0.85,
+                stream=False,
+                pad_token_id=tokenizer.pad_token_id
+            )
+token = outputs.squeeze()[input_ids.shape[1]:].tolist()
+print(tokenizer.decode(token,skip_special_tokens=True))
 # for token in model.generate(input_ids):
 #     print(tokenizer.decode(token))
 
-tokenizer = AutoTokenizer.from_pretrained('/home/bmh/project/llm_related/train_llm/model/save/pretrain/minimind_base_pre/')
-model = MiniMindLM(LMConfig())
-ckp = "/home/bmh/project/llm_related/train_llm/model/save/pretrain/minimind_base_pre/pytorch_model.bin"
-state_dict = torch.load(ckp, map_location="cuda")
-model.load_state_dict(state_dict)
-input_data = [tokenizer.bos_token_id] + tokenizer.encode('马克思主义基本原理')
-input_ids = torch.tensor(input_data).unsqueeze(0)
+# tokenizer = AutoTokenizer.from_pretrained('/home/bmh/project/llm_related/train_llm/model/save/pretrain/minimind_base_pre/')
+# model = MiniMindLM(LMConfig())
+# ckp = "/home/bmh/project/llm_related/train_llm/model/save/pretrain/minimind_base_pre/pytorch_model.bin"
+# state_dict = torch.load(ckp, map_location="cuda")
+# model.load_state_dict(state_dict)
+# input_data = [tokenizer.bos_token_id] + tokenizer.encode('马克思主义基本原理')
+# input_ids = torch.tensor(input_data).unsqueeze(0)
 
