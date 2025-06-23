01eab8c5ecdf44d276713e4c2e3968d286381330
zhangyf <zhangyfcoder@163.com>
2025-06-23 22:40:18 +0800

    fix bug

--- nlp/input_method/src/process.py
+++ nlp/input_method/src/process.py
@@ -5,10 +5,23 @@ import jieba
 from tqdm import tqdm
 
 jieba.setLogLevel(jieba.logging.WARNING)
+
+
+def build_dataset(sentences,word2index):
+    # 构建并保存训练数据
+    indexed_sentences = [[word2index.get(word,0) for word in jieba.lcut(sentence)] for sentence in sentences]
+    dataset = []  # [{input:[1,2,3,4,5],target:6},{input:[2,3,4,5,6],target:7}]
+    for sentence in indexed_sentences:
+        for i in range(len(sentence) - config.SEQ_LEN):
+            input = sentence[i:i + config.SEQ_LEN]
+            target = sentence[i + config.SEQ_LEN]
+            dataset.append({'input': input, 'target': target})
+    return dataset
+
 def process():
     print("开始处理数据")
 
-    df = pd.read_json(config.RAW_DATA_DIR / 'synthesized_.jsonl',lines=True,orient='records').sample(frac=0.1)
+    df = pd.read_json(config.RAW_DATA_DIR / 'synthesized_.jsonl',lines=True,orient='records')
     print(df.head())
     # 抽取数据
     sentences = []
@@ -31,14 +44,14 @@ def process():
     print(f"词表大小: {len(vocab_list)}")
 
     word2index = {word:index for index, word in enumerate(vocab_list)}
-    indexed_train_sentences = [[word2index[word] for word  in jieba.lcut(sentence)] for sentence in train_sentences]
-    indexed_test_sentences = [[word2index[word] for word  in jieba.lcut(sentence)] for sentence in test_sentences]
+    # 构建并保存训练数据
+    train_dataset = build_dataset(train_sentences, word2index)
+    pd.DataFrame(train_dataset).to_json(config.PROCESSED_DATA_DIR / 'train.jsonl', lines=True,orient='records')
 
 
-
-
-
-
+    # 构建并保存训练数据
+    test_dataset = build_dataset(test_sentences, word2index)
+    pd.DataFrame(test_dataset).to_json(config.PROCESSED_DATA_DIR / 'test.jsonl', lines=True,orient='records')
 
     print("数据处理完成")
 
